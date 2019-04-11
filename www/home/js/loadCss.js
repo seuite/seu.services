15bf8955b57ba397598b5aeb063eacf91248bf40
Apple Chen <encofcomic@outlook.com>
2019-04-12 06:08:49 +0800

    rewrite homepage

---
+++ www/home/js/loadCss.js
@@ -0,0 +1,1 @@
+define([],function(){function loadCss(url,callback){var link=document.createElement("link");link.type="text/css",link.rel="stylesheet",link.id="loadCss-"+Date.now(),link.href=url,link.onload=callback,head.appendChild(link)}var head=document.getElementsByTagName("head")[0];return loadCss});
